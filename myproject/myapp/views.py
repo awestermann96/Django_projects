75babdea759322089999b418ba64b15172402140
Annabel Westermann <annabel.westermann@hotmail.co.uk>
2024-02-06 18:31:13 +0000

    style submit form

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -3,4 +3,15 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse('<h1>Hey, welcome</h1>')
+    context = {
+        'name': 'Patrick',
+        'age': 23,
+        'nationality': 'British'
+    }
+    return render(request, 'index.html', context)
+
+def counter(request):
+    words = request.POST['words1']
+    n_words = len(words.split())
+    return render(request, 'counter.html', 
+                  {'n_words': n_words})
