a86436f317b087e4975ea63cdcc2bea757732903
Annabel Westermann <annabel.westermann@hotmail.co.uk>
2024-02-23 18:14:19 +0000

    changed

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Feature
 
 # Create your views here.
 def index(request):
@@ -8,7 +9,18 @@ def index(request):
         'age': 23,
         'nationality': 'British'
     }
-    return render(request, 'index.html', context)
+    feature1 = Feature()
+    feature1.id = 0
+    feature1.name = 'Fast'
+    feature1.is_true = True
+    feature1.details = 'Our service is very quick'
+
+    features = Feature.objects.all()
+
+    #features = [feature1, feature2, feature3, feature4]
+
+    return render(request, 'index.html', 
+                  {'features': features})
 
 def counter(request):
     words = request.POST['words1']
